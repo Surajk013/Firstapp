bf72e1b4874104dcc70074461b7ed4fa81b85a7e
Surajk013 <surajsinghk013@gmail.com>
2024-07-06 21:58:24 +0530

    2024-07-06

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+package com.first.metro;
+
+import android.app.activity;
+import android.os.bundle;
+
+public class MainActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.Activity.Main);
+	}
+}
+
